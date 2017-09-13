5eff47ba1cfc863c7ebc48113626b4f31a1c6ff2
opheJansen <ophe.jansen@gmail.com>
2017-09-13 15:34:03 +0200

    WIP tuto 'http://www.soluka.fr/blog/jeux/phaser-creer-flappy-bird-en-html5-canvas/'

---
+++ flappy-phaser/js/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'the-game', { preload: preload, create: create, update: update });
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
